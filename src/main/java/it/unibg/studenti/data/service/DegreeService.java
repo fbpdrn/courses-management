1dac3eba50327b6938d3029bbb8794a3e1337743
fbpdrn <git@fbpdrn.com>
2022-03-07 17:44:49 +0100

    Better type for methods in DegreeService.java.

--- src/main/java/it/unibg/studenti/data/service/DegreeService.java
+++ src/main/java/it/unibg/studenti/data/service/DegreeService.java
@@ -1,15 +1,16 @@
 package it.unibg.studenti.data.service;
 
+import it.unibg.studenti.generated.tables.records.CourseRecord;
+import it.unibg.studenti.generated.tables.records.DegreeHasCourseRecord;
 import it.unibg.studenti.generated.tables.records.DegreeRecord;
 import it.unibg.studenti.generated.tables.records.YearRecord;
 import org.jooq.DSLContext;
-import org.jooq.Record1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
 
 import static it.unibg.studenti.generated.tables.Course.COURSE;
 import static it.unibg.studenti.generated.tables.Degree.DEGREE;
@@ -77,15 +78,28 @@ public class DegreeService extends DatabaseService implements DatabaseDAO<Degree
                 .fetchInto(DegreeRecord.class);
     }
 
-    public Record1<BigDecimal> getCreditsAssigned(DegreeRecord degreeRecord) {
+    public Double getCreditsAssigned(DegreeRecord degreeRecord) {
         return getCreditsAssigned(degreeRecord.getIddegree());
     }
 
-    public Record1<BigDecimal> getCreditsAssigned(int degreeid) {
-        return getDSL().select(sum(COURSE.CREDITS))
+    public Double getCreditsAssigned(int degreeid) {
+        Double val = getDSL().select(sum(COURSE.CREDITS))
                 .from(COURSE)
                 .join(DEGREE_HAS_COURSE).on(DEGREE_HAS_COURSE.COURSE_IDCOURSE.eq(COURSE.IDCOURSE))
                 .join(DEGREE).on(DEGREE_HAS_COURSE.DEGREE_IDDEGREE.eq(DEGREE.IDDEGREE))
-                .where(DEGREE.IDDEGREE.eq(degreeid)).fetchOne();
+                .where(DEGREE.IDDEGREE.eq(degreeid)).fetchOneInto(Double.class);
+        return Objects.requireNonNullElse(val, 0.0);
+    }
+
+    public Integer addCourse(DegreeRecord degreeRecord, CourseRecord courseRecord) {
+        try {
+            DegreeHasCourseRecord record = new DegreeHasCourseRecord();
+            record.setDegreeIddegree(degreeRecord.getIddegree());
+            record.setCourseIdcourse(courseRecord.getIdcourse());
+            getDSL().insertInto(DEGREE_HAS_COURSE).set(record).execute();
+            return 1;
+        } catch(DuplicateKeyException e) {
+            return -1;
+        }
     }
 }
