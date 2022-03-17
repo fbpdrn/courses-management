0b2703c150f2fb5c9494c952c81efb5a1b449a30
fbpdrn <git@fbpdrn.com>
2022-03-17 05:41:57 +0100

    Added webdriver manager.

--- src/test/java/it/unibg/studenti/AbstractIT.java
+++ src/test/java/it/unibg/studenti/AbstractIT.java
@@ -2,11 +2,14 @@ package it.unibg.studenti;
 
 import com.vaadin.testbench.RetryRule;
 import com.vaadin.testbench.TestBenchTestCase;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import it.unibg.studenti.views.login.LoginViewElement;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
-import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public abstract class AbstractIT extends TestBenchTestCase {
 
@@ -15,7 +18,11 @@ public abstract class AbstractIT extends TestBenchTestCase {
 
     @Before
     public void setUp() {
-        setDriver(new SafariDriver());
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless", "--disable-gpu");
+        WebDriver chrome = new ChromeDriver(options);
+        setDriver(chrome);
     }
 
     @After
